40da2cecc12a33f32e27114eece95ffdab08a020
manjit <sidhum27@gmail.com>
2021-08-12 20:46:24 +0530

    web code added

---
+++ WebEndPoint/src/app/app-routing.module.ts
@@ -0,0 +1,11 @@
+import { Component, NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { EmployeeComponent } from './employee/employee.component';
+
+const routes: Routes = [{path:'Home', component: EmployeeComponent}];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
